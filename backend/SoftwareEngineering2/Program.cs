a7c769b880668a4afbcf6516242dfbcac446d3c0
Filip Kołodziejczyk <filip.kolodziejczyk.00@gmail.com>
2023-03-31 16:49:41 +0200

    Added db context

--- backend/SoftwareEngineering2/Program.cs
+++ backend/SoftwareEngineering2/Program.cs
@@ -1,10 +1,19 @@
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SoftwareEngineering2;
+using ConfigurationManager = System.Configuration.ConfigurationManager;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<FlowerShopContext>(
+    options => 
+        options.UseSqlServer(configuration.GetConnectionString("Db")));
 
 var app = builder.Build();
 
