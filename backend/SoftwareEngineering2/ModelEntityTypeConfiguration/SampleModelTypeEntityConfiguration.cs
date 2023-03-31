3d8c17401fb3a2f7a498a5af642f3e8046a327ff
Filip Kołodziejczyk <filip.kolodziejczyk.00@gmail.com>
2023-03-31 16:49:33 +0200

    Added sample model entity config

---
+++ backend/SoftwareEngineering2/ModelEntityTypeConfiguration/SampleModelTypeEntityConfiguration.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SoftwareEngineering2.Models;
+
+namespace SoftwareEngineering2.ModelEntityTypeConfiguration; 
+
+public class SampleModelTypeEntityConfiguration : IEntityTypeConfiguration<SampleModelType> {
+    public void Configure(EntityTypeBuilder<SampleModelType> builder) {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).IsRequired();
+    }
+}
