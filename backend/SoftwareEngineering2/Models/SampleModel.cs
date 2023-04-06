2cbcbe5b094358ceb7dfb63ae20a1d54efac2640
Filip Kołodziejczyk <filip.kolodziejczyk.00@gmail.com>
2023-04-06 11:02:33 +0200

    Added data annotations

--- backend/SoftwareEngineering2/Models/SampleModel.cs
+++ backend/SoftwareEngineering2/Models/SampleModel.cs
@@ -1,11 +1,17 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace SoftwareEngineering2.Models;
 
+[Table("Samples")]
 public class SampleModel {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
     public int Id { get; set; }
+    [Column(TypeName = "VARCHAR(255)")]
     [Required]
     public string Name { get; set; }
     
-    [Required]
+    [ForeignKey("SampleModelTypeId")]
     public SampleModelType Type { get; set; }
 }
--- backend/SoftwareEngineering2/Models/SampleModelType.cs
+++ backend/SoftwareEngineering2/Models/SampleModelType.cs
@@ -1,9 +1,14 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SoftwareEngineering2.Models; 
 
+[Table("SampleModelTypes")]
 public class SampleModelType {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
     public int Id { get; set; }
+    [Column(TypeName = "VARCHAR(255)")]
     [Required]
     public string Name { get; set; }
 }
