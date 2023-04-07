98808f0a78e9677eaf52139831529de7913ad3fc
Filip Kołodziejczyk <filip.kolodziejczyk.00@gmail.com>
2023-04-07 17:12:06 +0200

    Set proper life cycle of services

--- backend/SoftwareEngineering2/Program.cs
+++ backend/SoftwareEngineering2/Program.cs
@@ -1,19 +1,33 @@
 using System.Configuration;
+using System.Data;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using SoftwareEngineering2;
+using SoftwareEngineering2.Interfaces;
+using SoftwareEngineering2.Repositories;
 using ConfigurationManager = System.Configuration.ConfigurationManager;
 
 var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
+var connectionString = Environment.GetEnvironmentVariable("CONNSTR");
+if (string.IsNullOrEmpty(connectionString)) {
+    connectionString = ConfigurationManager.ConnectionStrings["Db"].ConnectionString;
+}
+
+builder.Services.AddTransient<IDbConnection>(_ => new SqlConnection(connectionString));
 builder.Services.AddDbContext<FlowerShopContext>(
-    options => 
-        options.UseSqlServer(configuration.GetConnectionString("Db")));
+    options => options.UseSqlServer(connectionString));
+
+
+builder.Services.AddTransient<ISampleModelRepository, SampleModelRepository>();
+builder.Services.AddTransient<ISampleModelTypeRepository, SampleModelTypeRepository>();
 
 var app = builder.Build();
 
