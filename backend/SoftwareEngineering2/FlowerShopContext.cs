cf473dd54794c846c037ae7f1c3bfa89acaf85f4
Filip Kołodziejczyk <filip.kolodziejczyk.00@gmail.com>
2023-04-08 00:25:05 +0200

    Initial DB migration

--- backend/SoftwareEngineering2/FlowerShopContext.cs
+++ backend/SoftwareEngineering2/FlowerShopContext.cs
@@ -8,4 +8,14 @@ public class FlowerShopContext : DbContext {
     
     public DbSet<SampleModel> SampleModels { get; set; }
     public DbSet<SampleModelType> SampleModelTypes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(FlowerShopContext).Assembly);
+        
+        modelBuilder.Entity<SampleModelType>().HasData(
+            new SampleModelType { Id = 1, Name = "Type 1" },
+            new SampleModelType { Id = 2, Name = "Type 2" },
+            new SampleModelType { Id = 3, Name = "Type 3" }
+        );
+    }
 }
