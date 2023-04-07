72cadf47384fa2b3c997737ac54df38de33b5df9
Filip Kołodziejczyk <filip.kolodziejczyk.00@gmail.com>
2023-04-07 15:56:40 +0200

    Fixed db settings (moved localhost conn string from prod to dev appsettings)

--- backend/SoftwareEngineering2/Models/SampleModel.cs
+++ backend/SoftwareEngineering2/Models/SampleModel.cs
@@ -3,7 +3,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SoftwareEngineering2.Models;
 
-[Table("Samples")]
 public class SampleModel {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     [Key]
--- backend/SoftwareEngineering2/Models/SampleModelType.cs
+++ backend/SoftwareEngineering2/Models/SampleModelType.cs
@@ -3,7 +3,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SoftwareEngineering2.Models; 
 
-[Table("SampleModelTypes")]
 public class SampleModelType {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     [Key]
