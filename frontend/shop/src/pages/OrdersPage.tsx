b8901628821ec0e1a047d167bdb083d7c2659915
Błażej Misiura <blazej.misiura@outlook.com>
2023-04-25 20:57:15 +0200

    shop page layout finished

--- frontend/shop/src/pages/OrdersPage.tsx
+++ frontend/shop/src/pages/OrdersPage.tsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+import OrderItem from "../components/OrderItem";
 
 function OrdersPage  ()  {  
   
   return (
-    <div>
-        orders
+    <div className="flex flex-col items-center justify-center h-screen w-5/6 p-4 bg-gray-300">
+        <div className="h-full overflow-y-auto w-full">
+            <OrderItem></OrderItem>
+            <OrderItem></OrderItem>
+            <OrderItem></OrderItem>
+        </div>
     </div>
     )
 };
