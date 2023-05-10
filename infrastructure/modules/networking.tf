881759b8d111bd5a28b1fdd3ec3e36d64718a673
Filip Kołodziejczyk <filip.kolodziejczyk.00@gmail.com>
2023-05-10 19:02:31 +0200

    Initial terraform setup (provider + vars + networking)

---
+++ infrastructure/modules/networking.tf
@@ -0,0 +1,38 @@
+resource "aws_internet_gateway" "aws-igw" {
+  vpc_id = aws_vpc.aws-vpc.id
+  
+  tags = {
+    Name        = "${var.app_name}-igw"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id      = aws_vpc.vpc.id
+  cidr_block  = var.cidr
+
+  tags  = {
+    Name        = "${var.app_name}-public-subnet"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.aws-vpc.id
+
+  tags = {
+    Name        = "${var.app_name}-routing-table-public"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.aws-igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,31 @@
+variable "aws_access_key" {
+  type        = string
+  sensitive   = true
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  sensitive   = true
+  description = "AWS Secret Key"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "app_name" {
+  type        = string
+  description = "Application Name"
+}
+
+variable "app_environment" {
+  type        = string
+  description = "Application Environment"
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC."
+  default     = "10.0.0.0/16"
+}
