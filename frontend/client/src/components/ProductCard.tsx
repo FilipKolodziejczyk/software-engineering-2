36bcbd43edecf8025d002a8bd60c006e9abaa04c
Adam Kwiatkowski <adam.kwiatkowski256@gmail.com>
2023-05-18 01:49:53 +0200

    Change the `image` field to a single string

--- frontend/client/src/components/ProductCard.tsx
+++ frontend/client/src/components/ProductCard.tsx
@@ -6,7 +6,7 @@ export default function ProductCard({product}: { product: Product }) {
   return (<Link className="group relative" to={`/products/${product.productID}`}>
       <div
         className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-xl bg-gray-200 border border-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-        <img src={product.image[0]}
+        <img src={product.image}
              alt={product.name}
              className="h-full w-full object-cover object-center lg:h-full lg:w-full cursor-pointer text-gray-200"/>
       </div>
