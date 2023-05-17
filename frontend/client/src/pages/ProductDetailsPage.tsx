060807f9202ece5118a14df7c1d958f9cdc66b28
Adam Kwiatkowski <adam.kwiatkowski256@gmail.com>
2023-05-17 22:42:21 +0200

    Add "Add to cart" functionality

--- frontend/client/src/pages/ProductDetailsPage.tsx
+++ frontend/client/src/pages/ProductDetailsPage.tsx
@@ -11,8 +11,27 @@ export async function loader({params}: any) {
 
 export default function ProductDetailsPage() {
   const [quantity, setQuantity] = React.useState(1);
-
   const {product}: any = useLoaderData();
+
+  const addToCart = async () => {
+    const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/basket`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        productId: product.id,
+        quantity: quantity
+      })
+    });
+
+    if (response.ok) {
+      alert('Added to cart');
+    } else {
+      alert('Failed to add to cart');
+    }
+  }
+
   return (<div className="mx-auto max-w-2xl px-4 py-4 pb-12 sm:px-6 sm:py-8 lg:max-w-5xl 2xl:max-w-7xl lg:px-8">
     <div className="lg:grid lg:grid-cols-2 lg:gap-x-16 lg:items-start">
       <div className="mb-4">
@@ -45,7 +64,8 @@ export default function ProductDetailsPage() {
 
 
             <button type="button"
-                    className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-neutral-950 hover:bg-neutral-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900">
+                    className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-neutral-950 hover:bg-neutral-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                    onClick={addToCart}>
               Add to cart
             </button>
           </div>
