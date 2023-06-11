084c175dcbed698b2628f313360876e6e12af772
jacek <zalewskii.jacek@gmail.com>
2023-06-11 12:45:37 +0200

    paging added

--- backend/SoftwareEngineering2Test/Services/ProductServiceTests.cs
+++ backend/SoftwareEngineering2Test/Services/ProductServiceTests.cs
@@ -7,44 +7,51 @@ using SoftwareEngineering2.Profiles;
 using AutoMapper;
 using SoftwareEngineering2.DTO;
 
-namespace SoftwareEngineering2.Services.Tests {
+namespace SoftwareEngineering2.Services.Tests
+{
     [TestFixture()]
-    public class ProductServiceTests {
+    public class ProductServiceTests
+    {
         private static IMapper _mapper = null!;
         private static IUnitOfWork _unitOfWork = null!;
         private static readonly Mock<IProductRepository> mockRepo = new();
         private static ProductService _productService = null!;
 
-        public ProductServiceTests() {
-            if (_mapper is null) {
-                var mappingConfig = new MapperConfiguration(mc => {
-                    mc.AddProfile(new AutoMapperProfile());
-                });
+        public ProductServiceTests()
+        {
+            if (_mapper is null)
+            {
+                var mappingConfig = new MapperConfiguration(mc => { mc.AddProfile(new AutoMapperProfile()); });
                 IMapper mapper = mappingConfig.CreateMapper();
                 _mapper = mapper;
             }
 
-            if (_unitOfWork is null) {
+            if (_unitOfWork is null)
+            {
                 var mockUnit = new Mock<IUnitOfWork>();
                 _unitOfWork = mockUnit.Object;
             }
 
-            mockRepo.Setup(e => e.GetByIdAsync(4)).Returns(Task.FromResult((ProductModel?) new ProductModel {
+            mockRepo.Setup(e => e.GetByIdAsync(4)).Returns(Task.FromResult((ProductModel?)new ProductModel
+            {
                 ProductID = 4, Name = "Rose", Description = "String", Archived = false,
                 Category = "flowers", Image = "", Price = 5, Quantity = 10
             }));
-
-            if (_productService is null) {
+            
+            if (_productService is null)
+            {
                 var mockRepoObj = mockRepo.Object;
                 _productService = new(_unitOfWork, mockRepoObj, _mapper);
             }
         }
 
         [Test()]
-        public async Task GetModelByIdAsyncTest() {
+        public async Task GetModelByIdAsyncTest()
+        {
             var dto = await _productService.GetModelByIdAsync(4);
 
-            Assert.That(dto, Is.EqualTo(new ProductDTO {
+            Assert.That(dto, Is.EqualTo(new ProductDTO
+            {
                 Archived = false,
                 Category = "flowers",
                 Description = "String",
@@ -57,9 +64,24 @@ namespace SoftwareEngineering2.Services.Tests {
 
             Assert.That(await _productService.GetModelByIdAsync(6), Is.Null);
         }
-
+        
+        [Test()]
+        public async Task GetAllFilteredAsyncTest()
+        {
+            Assert.Pass();
+        }
         [Test()]
         public void CreateModelAsyncTest() {
+            Assert.Pass();
+        }
+        [Test()]
+        public void DeleteModelAsyncTest() {
+            Assert.Pass();
+        }
+        
+        [Test()]
+        public void UpdateModelAsyncTest()
+        {
             Assert.Pass();
         }
     }
--- backend/SoftwareEngineering2Test/UnitTest1.cs
+++ backend/SoftwareEngineering2Test/UnitTest1.cs
@@ -9,9 +9,9 @@ public class Tests {
     [SetUp]
     public void Setup() { }
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
+    // [Test]
+    // public void Test1()
+    // {
+    //     Assert.Pass();
+    // }
 }
